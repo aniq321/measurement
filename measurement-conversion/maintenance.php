4b9b04ebff5ea84d8d8b038c6df59cdf2accc6f0
aniq321 <fnaticniq@gmail.com>
2023-04-15 16:15:51 +0800

    save

--- measurement-conversion/maintenance.php
+++ measurement-conversion/maintenance.php
@@ -5,11 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Convert Length</title>
+    <link rel="stylesheet" type="text/css" href="Styles.css">
 </head>
 <body>
     <div id="main-content">
         <h1 style="text-align: center;">OH NO !</h1>
-        <img src="maintanance.jpg" alt="">
+        <img src="maintanance.jpg" alt="" class="center">
         <p style="text-align: center;">Return to <a href="index.php">Main Menu</a></p>
     </div>
 </body>
